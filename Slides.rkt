8bdc5f3e7fe3efb872017b3246506ea944c70777
Merve Tuccar <merve_tuccar@student.uml.edu>
2015-04-13 18:48:05 -0400

    Update Slides.rkt

--- Slides.rkt
+++ Slides.rkt
@@ -1,5 +1,15 @@
-#lang slideshow
+;;Alex Nevers
+;;Merve Tuccar
 
+#lang slideshow
+;;notes ->>
+;;add animation
+;;add more styling
+;;add pictures
+
+(current-font-size 32)
+(current-main-font "Verdana")
+(current-title-color "blue")
 
 (slide
  #:title "CYBERDYNE SYSTEMS PRESENTS"
@@ -9,33 +19,46 @@
  #:title "CREATING THE FUTURE OF TECHNOLOGY"
  (para "CYBERDYNE SYSTEMS has been working with the American people, for the American people, for over a decade. Within this decade we've pioneered new breakthroughs in")
  (item "Artificial Intelligence")
+ 'next
  (item "Neural Network Processors")
+ 'next
  (item "Advanced Robotic Systems for Medicine")
+ 'next
  (item "Consumer Products")
+ 'next
  (item "Defense"))
 
 (slide
  #:title "WHAT LIES AHEAD"
  (t "Our greatest challenge lies ahead. ")
  (t "Or rather... above. ")
- (para "Six hundred miles above, in geosynchronous earth orbit.  Today, these CYBERDYNE satellites protect us from enemy attack.  But soon, they'll go even farther. Imagine every computer on earth… Every television... Every telephone... and eventually, every living person... united.  In the ultimate global network. ")
+ (para "Six hundred miles above, in geosynchronous earth orbit.  Today, these CYBERDYNE satellites protect us from enemy attack.  But soon, they'll go even farther. Imagine every computer on earth... Every television... Every telephone... and eventually, every living person... united.  In the ultimate global network. ")
  (t "You've heard of the information highway?")
+ 'next
+ 
  (t "Welcome to the information skyway.")
  )
 
 (slide
  #:title "INTRODUCING: SKYNET"
  (t "This is the future of communication.")
+ 'next
  (t "The future of National Defense.")
+ 'next
  (t "The future of a new global society.")
+ 'next
  (t "Ladies and Gentlemen...")
+ 'next
  (t "This is SKYNET."))
 
 (slide
  #:title "WHAT IS SKYNET?"
  (t "SKYNET is:")
+ 'next
  (item "The world's first Automated Defense Network, processing information at ninety teraflops. ")
+ 'next
  (item "The controlling force behind all CYBERDYNE satellites. SKYNET pools data from satellites, develops tactics and coordinates attacks.")
+ 'next
  (item "The control over everything which contains a Cyberdyne Systems CPU. "))
 
 (slide
