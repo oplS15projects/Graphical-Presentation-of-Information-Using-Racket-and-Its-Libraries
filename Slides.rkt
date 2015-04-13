44da469b20838fb91bd90d1a4c8292571e1f0a78
Alex Nevers <alexandra.nevers@gmail.com>
2015-04-13 17:20:08 -0400

    Slides.rkt

---
+++ Slides.rkt
@@ -0,0 +1,66 @@
+#lang slideshow
+
+
+(slide
+ #:title "CYBERDYNE SYSTEMS PRESENTS"
+ (t "S k y n e t: The Future of A.I."))
+
+(slide
+ #:title "CREATING THE FUTURE OF TECHNOLOGY"
+ (para "CYBERDYNE SYSTEMS has been working with the American people, for the American people, for over a decade. Within this decade we've pioneered new breakthroughs in")
+ (item "Artificial Intelligence")
+ (item "Neural Network Processors")
+ (item "Advanced Robotic Systems for Medicine")
+ (item "Consumer Products")
+ (item "Defense"))
+
+(slide
+ #:title "WHAT LIES AHEAD"
+ (t "Our greatest challenge lies ahead. ")
+ (t "Or rather... above. ")
+ (para "Six hundred miles above, in geosynchronous earth orbit.  Today, these CYBERDYNE satellites protect us from enemy attack.  But soon, they'll go even farther. Imagine every computer on earth… Every television... Every telephone... and eventually, every living person... united.  In the ultimate global network. ")
+ (t "You've heard of the information highway?")
+ (t "Welcome to the information skyway.")
+ )
+
+(slide
+ #:title "INTRODUCING: SKYNET"
+ (t "This is the future of communication.")
+ (t "The future of National Defense.")
+ (t "The future of a new global society.")
+ (t "Ladies and Gentlemen...")
+ (t "This is SKYNET."))
+
+(slide
+ #:title "WHAT IS SKYNET?"
+ (t "SKYNET is:")
+ (item "The world's first Automated Defense Network, processing information at ninety teraflops. ")
+ (item "The controlling force behind all CYBERDYNE satellites. SKYNET pools data from satellites, develops tactics and coordinates attacks.")
+ (item "The control over everything which contains a Cyberdyne Systems CPU. "))
+
+(slide
+ #:title "SAFETY FROM THE SKIES"
+ (para "With the entirety of CYNDERDYNE’s satellites at it’s fingertips, SKYNET has complete control of the skies. With this power, SKYNET can detect aerial attacks from enemy countries the instant they are launched.")
+ (para "SKYNET can can calculate exact trajectory paths from space. These paths can then be ray traced onto precise maps for strategic use by our world leaders."))
+
+(slide
+ #:title "IMPENETRABLE SUBTERRANEAN CORE"
+ (para "The SKYNET base is located in Cheyenne Mountain, Colorado, the world's most heavily armored and defended mountain. Hollowed out, reinforced and armored, Cheyenne Mountain is capable of withstanding a direct hit from a nuclear missile. ")
+ )
+
+(slide
+ #:title "STATE OF THE ART A.I."
+ (para "CYBERDYNE has compiled our most intelligent and innovative programmers to work on SKYNET. After years of work, they have created the perfect, interconnected network that can think for itself.")
+ (para "SKYNET can learn at a geometric rate. Though it is not intelligent enough to be self-aware, it is the ultimate processor and relies on human input for decisions."))
+
+(slide
+ #:title "OPERATIONS BEGIN SOON"
+ (para "With CYBERDYNE’s innovation and tech, SKYNET is set to be the safety net for the world. Global conflict will stalemate in the face of this technological giant, ever-watching from the skies.")
+ (t "SKYNET goes online on August 4, 1997.")
+ (t "Your future is in our trusted hands." ))
+
+(slide
+ (t "Making the future safer and friendlier... for everyone.")
+ (t "© 1997 CYBERDYNE SYSTEMS ALL RIGHTS RESERVED")
+ (t "18144 El Camino Real, Sunnyvale, California")
+ (t "https://www.linkedin.com/company/cyberdyne-systems-corporation"))
