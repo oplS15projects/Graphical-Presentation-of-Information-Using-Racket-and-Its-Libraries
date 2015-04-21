5a5e061efd21cae4bc6e13bfbb75de80ef62cc73
Alex Nevers <alexandra.nevers@gmail.com>
2015-04-21 16:50:50 -0400

    Update Slides.rkt

--- Slides.rkt
+++ Slides.rkt
@@ -2,8 +2,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;Graphical-Presentation-of-Information-Using-Racket-and-Its-Libraries
-<<<<<<< HEAD
-=======
 
 ;;Team Lapsus
 
@@ -41,76 +39,14 @@
 ;;Alt-p, Cmd-p, or Meta-p: show/hide slide number
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;notes to do->>
-;;add animation
-;;add more styling
-;;add pictures
 
 (current-font-size 32)
 (current-main-font "Verdana")
 (current-title-color "blue")
 
 (define (run)
->>>>>>> origin/master
 
-;;Team Lapsus
-
-;;Alex Nevers & Merve Tuccar
-;;OPL Spring 2015 Final Project
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;; NOTES:
-;;******type (run) in cmnd line to run******
-
-;;if you use (spacebar) to skip through slides, they will appear out of order
-;;if you let them timeout, they will be in order
-
-;;change timeout to 2 if you want to do a quick lookover in order
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;;SLIDE COMMANDS:
-;;;;;Ending:
-
-;;Alt-q, Meta-q, or Cmd-q:                end slide show
-;;Esc:                                    if confirmed, end show
-
-;;;;Navigation:
-
-;;Right arrow, Space, f, n, or click:     next slide
-;;Left arrow, Backspace, Delete, or b:    previous slide
-;;g:                                      last slid  
-;;1:                                      first slide
-
-;;;;Misc:
-
-;;Alt-g, Cmd-g, or Meta-g: select a slide
-;;Alt-p, Cmd-p, or Meta-p: show/hide slide number
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-
-
-(define (run)
-  
-(slide
-<<<<<<< HEAD
- #:timeout 20 
- (t "CYBERDYNE SYSTEMS PRESENTS")
- (t "S k y n e t: The Future of A.I."))
-
-(slide
- #:title "CREATING THE FUTURE OF TECHNOLOGY"
- #:timeout 20
- (para "CYBERDYNE SYSTEMS has been working with the American people, for the American people, for over a decade. Within this decade we've pioneered new breakthroughs in")
- (item "Artificial Intelligence")
- (item "Neural Network Processors")
- (item "Advanced Robotic Systems for Medicine")
- (item "Consumer Products")
- (item "Defense"))
-=======
+(slide 
  #:title "CYBERDYNE SYSTEMS PRESENTS"
  #:timeout 20 
  (bitmap "Cyberdyne_logo_OLD.jpg" )
@@ -136,24 +72,30 @@
                                     (item "Defense"))))))
 
 (current-font-size 28)
->>>>>>> origin/master
-
 
 (slide
  #:title "WHAT LIES AHEAD"
-<<<<<<< HEAD
  #:timeout 20
- (para "Our greatest challenge lies ahead. ")
- (para "Or rather... above. ")
- (para "Six hundred miles above, in geosynchronous earth orbit.  Today, these CYBERDYNE satellites protect us from enemy attack.  But soon, they'll go even farther. Imagine every computer on earth... Every television... Every telephone... and eventually, every living person... united.  In the ultimate global network. ")
- (para "You've heard of the information highway?")
- (para "Welcome to the information skyway.")
- )
+ #:gap-size 19
+ 
+ (lt-superimpose 
+   (hc-append 
+             (para #:align 'left #:fill? #t #:width 550 "         ")
+             (bitmap "T23d-skynetsatellite-poster.png"))
+             
+    (vl-append (para #:align 'left #:fill? #t #:width 540 "Our greatest challenge lies ahead. ")
+       (vl-append 
+                  (vl-append (para #:align 'left #:fill? #t #:width 540 "Or rather... above. ")
+                             (vl-append (t " ")
+                                        (para  #:align 'left #:fill? #t #:width 540 "Six hundred miles above, in geosynchronous earth orbit. Today, these" (tt "CYBERDYNE") "satellites protect us from enemy attack. But soon, they'll go even farther. Imagine every computer on earth, every television, every telephone, and eventually, every living person... united. In the ultimate global network.")))
+                  (vl-append (t " ")
+                             (vl-append (para #:align 'left #:fill? #t #:width 540 "You've heard of the information highway?")
+                                        (para  #:align 'left #:fill? #t #:width 540 "Welcome to the information" (it "skyway."))))))))
+
+  
 
 (slide
  #:title "INTRODUCING: SKYNET"
- #:timeout 20
-=======
  #:timeout 20  #:gap-size 19
  
  (lt-superimpose 
@@ -175,7 +117,7 @@
 (slide
  #:title "INTRODUCING: SKYNET"
  #:timeout 20  #:gap-size 10
->>>>>>> origin/master
+
  (t "This is the future of communication.")
  (t "The future of National Defense.")
  (t "The future of a new global society.")
@@ -187,24 +129,6 @@
   
 (slide
  #:title "WHAT IS SKYNET?"
-<<<<<<< HEAD
- #:timeout 20
- (para "SKYNET is:")
- (item "The world's first Automated Defense Network, processing information at ninety teraflops. ")
- (item "The controlling force behind all CYBERDYNE satellites. SKYNET pools data from satellites, develops tactics and coordinates attacks.")
- (item "The control over everything which contains a Cyberdyne Systems CPU. "))
-
-(slide
- #:title "SAFETY FROM THE SKIES"
- #:timeout 20
- (para "With the entirety of CYNDERDYNE’s satellites at it’s fingertips, SKYNET has complete control of the skies. With this power, SKYNET can detect aerial attacks from enemy countries the instant they are launched.")
- (para "SKYNET can can calculate exact trajectory paths from space. These paths can then be ray traced onto precise maps for strategic use by our world leaders."))
-
-(slide
- #:title "IMPENETRABLE SUBTERRANEAN CORE"
- #:timeout 20
- (para "The SKYNET base is located in Cheyenne Mountain, Colorado, the world's most heavily armored and defended mountain. Hollowed out, reinforced and armored, Cheyenne Mountain is capable of withstanding a direct hit from a nuclear missile. ")
-=======
  #:timeout 20 
 (para (tt "SKYNET") "is:")
 
@@ -231,28 +155,10 @@
  #:timeout 2
  (para #:width 900 "The" (tt "SKYNET") "base is located in Cheyenne Mountain, Colorado, the world's most heavily armored and defended mountain. Hollowed out, reinforced and armored, Cheyenne Mountain is capable of withstanding a direct hit from a nuclear missile. ")
  (bitmap "T23d-skynet-core.png")
->>>>>>> origin/master
  )
 
 (slide
  #:title "STATE OF THE ART A.I."
-<<<<<<< HEAD
- #:timeout 20
- (para "CYBERDYNE has compiled our most intelligent and innovative programmers to work on SKYNET. After years of work, they have created the perfect, interconnected network that can think for itself.")
- (para "SKYNET can learn at a geometric rate. Though it is not intelligent enough to be self-aware, it is the ultimate processor and relies on human input for decisions."))
-
-(slide
- #:title "OPERATIONS BEGIN SOON"
- #:timeout 20
- (para "With CYBERDYNE’s innovation and tech, SKYNET is set to be the safety net for the world. Global conflict will stalemate in the face of this technological giant, ever-watching from the skies.")
- (t " ")
- (t " ")
- (t "SKYNET goes online on August 4, 1997.")
- (t "Your future is in our trusted hands." ))
-
-(slide
- #:timeout 20
-=======
  #:timeout 20 
 (lt-superimpose 
    (hc-append
@@ -283,7 +189,6 @@
 
 (slide
 #:timeout 20 
->>>>>>> origin/master
  (t "Making the future safer and friendlier... for everyone.")
  (t "  ")
  (lt-superimpose
@@ -298,14 +203,6 @@
  
          
  (t "https://www.linkedin.com/company/cyberdyne-systems-corporation"))
-<<<<<<< HEAD
   
   (run))
-=======
 
-
- (run))
- 
- 
- 
->>>>>>> origin/master
